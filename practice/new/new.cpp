35f77fbcd301dbe97f460e735f44f8420bcf0b39
Tejas Gund <tejasgund111@gmail.com>
2021-11-18 15:44:18 +0530

    friend function created

--- practice/new/new.cpp
+++ practice/new/new.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class name{
-    
-    public:
-    static int b;
-    static void fun(int a){
-        cout <<"value of a is : "<<a<<endl;
-        cout<<"Value of b is "<<b<<endl;
-    }
+class name
+{
+  int b;
 
-    
+public:
+  friend void fun1(name);
 };
-int name:: b=8;
-int main(){
-    name::fun(7);
-    return 0;
+void fun1(name c)
+{
+  int t=c.b;
+  t = 90;
+  cout << "value of b is " << t << endl;
+}
+int main()
+{
+  name obj;
+
+  fun1(obj);
+  return 0;
 }
