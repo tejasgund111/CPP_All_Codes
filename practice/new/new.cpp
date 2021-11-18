1371469bf834090097d6b6f9a124000f08ecf34d
Tejas Gund <tejasgund111@gmail.com>
2021-11-18 11:36:21 +0530

    new changes

--- practice/new/new.cpp
+++ practice/new/new.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
+class name{
+    private:
+    int a,b;
+    public:
+    name(int a1,int b1){
+        a=a1;
+        b=b1;
+    }
+    // name(name &p1){
+    //     a=p1.a;
+    //     b=p1.b;
+    // }
 
+    int getA(){
+        return a;
+    }
+    int getB(){
+        return b;
+    }
+};
 int main(){
-   int a;
-   cin>>a;
-   cout<<"your number is "<<a<<endl;
+    name c1(2,5);
+    name c2=c1;
+    cout<<"for c1 "<<c1.getA()<<" " <<c1.getB()<<endl;
+    cout<<"for c2 "<<c2.getA()<<" " <<c2.getB()<<endl;
+
     return 0;
 }
