663602388824fb49b2ffe803afe944c061f886d5
Tejas Gund <tejasgund111@gmail.com>
2021-11-18 12:30:20 +0530

    static datamember created

--- practice/new/new.cpp
+++ practice/new/new.cpp
@@ -3,12 +3,15 @@ using namespace std;
 class name{
     
     public:
+    static int b;
     static void fun(int a){
         cout <<"value of a is : "<<a<<endl;
+        cout<<"Value of b is "<<b<<endl;
     }
 
     
 };
+int name:: b=8;
 int main(){
     name::fun(7);
     return 0;
