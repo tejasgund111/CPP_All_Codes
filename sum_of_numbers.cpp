32feedd7e0b23375d8cc05e736d16ade9f79639f
Tejas Gund <tejasgund111@gmail.com>
2021-11-18 22:37:00 +0530

    new code added

---
+++ sum_of_numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the number upto which you want to print the sum of numbers" << endl;
+    cin >> num;
+
+    int sum = 0;
+    int i = 1;
+
+    while (i <= num)
+    {
+        sum += i;
+        i++;
+    }
+
+    cout << "Value of sum is : " << sum << endl;
+    return 0;
+}
