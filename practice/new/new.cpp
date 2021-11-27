dbf2f851ac5c2c4de31613952cfbb266318fa092
Tejas Gund <tejasgund111@gmail.com>
2021-11-27 19:51:59 +0530

    Commited changes

--- practice/new/new.cpp
+++ practice/new/new.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
+class A{
+    public:
+    A(){
+        cout<<"This is a"<<endl;
+    }
+    A(int a){
+        cout<<"This is  for a"<<a<<endl;
+    }
 
+};
 int main(){
+    // A a();
+    A a();
+    A b(7);
     
     return 0;
 }
---
+++ practice/new/new2.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> arr;
+int x,y,z;
+void plusMinus()
+{
+    double p;
+    double z;
+    double neg;
+    int n;
+    cin >> n;
+
+    int positive = 0;
+    int negative = 0;
+    int zero = 0;
+    int i;
+    
+    for (i = 0; i < n; i++)
+    {
+        int m;
+        cin>>m;
+        arr.push_back(m);
+
+        if (arr[i] > 0)
+        {
+            x=positive++;
+        }
+        // if (arr[i] < 0)
+        // {
+        //     y=++negative;
+        // }
+        // if(arr[i]==0)
+        // {
+        //     z=++zero;
+        // }
+    }
+        int d =positive;
+        double pos = d/n;
+
+        cout<<fixed<<pos;
+        // p = x / n;
+        // neg = y / n;
+        // z = z / n;
+       
+
+    // cout<<fixed<<p<<endl;
+    // cout<<neg<<endl;
+    // cout<<z<<endl;
+}
+int main(){
+    plusMinus();
+    return 0;
+}
