f154d9d2a088d554dcc6218804043bd34f4fe1e8
Tejas Gund <tejasgund111@gmail.com>
2021-12-18 12:49:38 +0530

    Commited changes

---
+++ Love Babbar/sorting_algorithms/selection_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);cls
+    }
+    // sorted array...
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "  ";
+    }
+}
+
+int main()
+{
+    int n = 5;
+    int arr[n] = {5, 2, 6, 7, 3};
+    selectionSort(arr, n);
+
+    return 0;
+}
