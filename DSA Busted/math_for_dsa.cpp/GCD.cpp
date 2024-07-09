e27fa89408f901520875b28207dab251596176ef
Tejas Gund <tejasgund111@gmail.com>
2024-07-10 00:31:10 +0530

    Commited changes

--- DSA Busted/math_for_dsa.cpp/GCD.cpp
+++ DSA Busted/math_for_dsa.cpp/GCD.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int gcd(int a, int b)
@@ -25,17 +25,41 @@ int gcd(int a, int b)
     return a;
 }
 
+// int gcd(int a, int b)
+// {
+//     while (a > 0 && b > 0)
+//     {
+//         if (a > b)
+//             a = a % b;
+//         else
+//             b = b % a;
+//     }
+//     if (a == 0)
+//         return b;
+//     return a;
+// }
+
+int lcm(int a, int b)
+{
+    return (a * b) / (gcd(a, b));
+}
+
 int main()
 {
+    /*
+        int a, b;
+        cout << "Enter the Values of a and b" << endl;
+        cin >> a >> b;
 
-    int a, b;
-    cout << "Enter the Values of a and b" << endl;
-    cin >> a >> b;
+        int ans = gcd(a, b);
 
-    int ans = gcd(a, b);
+        cout << " The GCD of " << a << " & " << b << " is: " << ans << endl;
+    */
 
-    cout << " The GCD of " << a << " & " << b << " is: " << ans << endl;
+    int a = 5, b = 10;
+    cout << "gcd is -> " << gcd(a, b) << endl;
+    cout << "lcm is -> " << lcm(a, b) << endl;
 
     return 0;
 }
-//This is done using Euclid's algorithm
+// This is done using Euclid's algorithm
--- only cp/one.cpp
+++ only cp/one.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    
+    string s = "abcdefgh";
+    vector<int> arr = {1,2,3,4,5,6,7,8};
+    vector<int> arr2 = {1,2,3,4};
     return 0;
 }
